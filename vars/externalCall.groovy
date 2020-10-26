34ee32ad7f55671d48a3545656f10989d71fc0e7
Ramana <abhyramana@gmail.com>
2020-10-26 11:59:45 -0400

    added for params to display

--- vars/externalCall.groovy
+++ vars/externalCall.groovy
@@ -1,6 +1,8 @@
 def call(String whoAreYou) {
 	
 	echo "Build #,  ${env.BUILd_NUMBER}"
+	
+	echo "Params ::, ${params}"
 	echo "Now we're being called more magically, ${whoAreYou}, thanks to the call(...) method."
 	
 }
